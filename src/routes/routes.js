64ee18056de3b17e6fd4df404383c25df83c7f4d
Daniel Muñoz <daniel20025febrero29@gmail.com>
2025-10-30 21:29:06 -0500

    Add endpoint for instagram

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,9 +7,11 @@ import { upload } from "../middleware/multer.js";
 
 // Controllers
 import { getAllGallery, saveImageGallery } from "../controllers/gallery.controller.js"
+import { getImageWithProxy } from "../controllers/external.controller.js";
 
 // Database
 import { getConnect } from "../database/connection.controller.js"
+
 
 const router = express();
 
@@ -17,6 +19,9 @@ export const routes = () => {
     // Routes Gallery
     router.get("/gallery/g/images", AuthorizationVerify, getAllGallery)
     router.post("/gallery/p/image", upload.single("image"), AuthorizationVerify, saveImageGallery)
+
+    //External Routes
+    router.get('/external/g/instagram/proxy-img', getImageWithProxy);
 
     // Database
     router.get("/connect/", ConnectionVerify, getConnect)
